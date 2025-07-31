d1ee2252fdbcfb98cb1e815051c579558b2f3e0e
hasaway2 <hasaway2@naver.com>
2025-07-31 11:16:58 +0900

    07-31

--- src/main/java/com/example/demo/entity/Level.java
+++ src/main/java/com/example/demo/entity/Level.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import com.fasterxml.jackson.annotation.*;
 import lombok.*;
 
 @Getter
@@ -9,4 +10,9 @@ public enum Level {
 
   // enum에 한글화 파라미터를 추가하면, 추가한 파라미터를 필드로
   private final String name;
+
+  @JsonValue
+  public String toJson() {
+    return this.name() + " (" + name + ")";
+  }
 }
